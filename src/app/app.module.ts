d34b4e7223106999053e18a64ed4c6f49ba98153
HugoErb <erb.hugo@yahoo.fr>
2023-06-15 00:17:48 +0200

    ajout du select, recup dans le json, design du select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
         BrowserAnimationsModule,
         FormsModule,
         AppRoutingModule,
-        AutoCompleteModule
+        AutoCompleteModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
