77c398b09df5208276cd19888b89deea8d3f5a21
HugoErb <erb.hugo@yahoo.fr>
2023-11-27 18:13:54 +0100

    add changement de prix en local storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -52,27 +52,13 @@ export class HomeComponent {
         const storedRunes = localStorage.getItem('runesData');
         if (storedRunes) {
             const runesData = JSON.parse(storedRunes);
-            this.initializeRunes(runesData);
+            this.runes = runesData;
         } else {
             this.http.get('assets/jsons/runes.json').subscribe((data: any) => {
                 localStorage.setItem('runesData', JSON.stringify(data));
-                this.initializeRunes(data);
+                this.runes = data;
             });
         }
-    }
-
-    initializeRunes(data: any[]) {
-        data.forEach((rune: any) => {
-            this.runes.push({
-                name: rune.Name,
-                stat: rune.Stat,
-                img: rune.Img,
-                price: rune.Price,
-                weight: rune.Weight,
-                raPrice: rune.RaPrice,
-                paPrice: rune.PaPrice,
-            });
-        });
     }
 
     private processData(data: any[]): any[] {
--- src/app/runes-manager/runes-manager.component.ts
+++ src/app/runes-manager/runes-manager.component.ts
@@ -19,32 +19,23 @@ export class RunesManagerComponent implements OnInit {
     const storedRunes = localStorage.getItem('runesData');
     if (storedRunes) {
       const runesData = JSON.parse(storedRunes);
-      this.initializeRunes(runesData);
+      this.runes = runesData;
+
     } else {
       this.http.get('assets/jsons/runes.json').subscribe((data: any) => {
         localStorage.setItem('runesData', JSON.stringify(data));
-        this.initializeRunes(data);
+        this.runes = data;
       });
     }
   }
 
-  initializeRunes(data: any[]) {
-    data.forEach((rune: any) => {
-      this.runes.push({
-        name: rune.Name,
-        stat: rune.Stat,
-        img: rune.Img,
-        price: rune.Price,
-        weight: rune.Weight,
-        raPrice: rune.RaPrice,
-        paPrice: rune.PaPrice,
-      });
-    });
+  onPriceChange(runeIndex: number, priceType: string, event: any) {
+    const newPrice = parseFloat(event.target.value);
+    this.runes[runeIndex][priceType] = newPrice;
   }
 
   saveChanges(): void {
-    // this.runeService.updateRunes(this.runes).subscribe(result => {
-    //   // Gérer le retour de l'opération
-    // });
+    console.log(this.runes);
+    localStorage.setItem('runesData', JSON.stringify(this.runes));
   }
 }
