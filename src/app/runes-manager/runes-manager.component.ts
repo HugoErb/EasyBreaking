ea68d727ec747cad46ac72e7f6a2c468f023a504
HugoErb <erb.hugo@yahoo.fr>
2023-11-27 21:46:47 +0100

    reset rune price + style btn

--- src/app/runes-manager/runes-manager.component.ts
+++ src/app/runes-manager/runes-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-runes-manager',
@@ -9,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class RunesManagerComponent implements OnInit {
   runes: any[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.loadRunes();
@@ -36,6 +37,11 @@ export class RunesManagerComponent implements OnInit {
 
   saveChanges(): void {
     localStorage.setItem('runesData', JSON.stringify(this.runes));
+    this.goToHomePage()
+  }
+
+  goToHomePage() {
+    this.router.navigate(['']); // Utilisez le chemin approprié pour votre page d'accueil
   }
 
   /**
@@ -46,9 +52,9 @@ export class RunesManagerComponent implements OnInit {
    */
   copyToClipboard(event: MouseEvent, runeName: string): void {
     navigator.clipboard.writeText(runeName).then(() => {
-        console.log(`Copié dans le presse-papiers: ${runeName}`);
+      console.log(`Copié dans le presse-papiers: ${runeName}`);
     }).catch(err => {
-        console.error('Erreur lors de la copie dans le presse-papiers: ', err);
+      console.error('Erreur lors de la copie dans le presse-papiers: ', err);
     });
 
     // Ajoute un focus pour faire apparaître la tooltip
@@ -57,5 +63,5 @@ export class RunesManagerComponent implements OnInit {
 
     // Retire le focus après 2 secondes
     setTimeout(() => element.blur(), 1500);
-}
+  }
 }
